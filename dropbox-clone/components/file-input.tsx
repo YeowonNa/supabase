cfc10d07232dfe8c4a3481e28d1e53c94986a813
yeowonNa <mhyh716@daum.net>
2024-10-08 10:51:42 +0900

    Drag&Drop, 멀티파일 업로드 구현

--- dropbox-clone/components/file-input.tsx
+++ dropbox-clone/components/file-input.tsx
@@ -1,13 +1,33 @@
 "use client";
 
-import { Button } from "@material-tailwind/react";
+import { Spinner } from "@material-tailwind/react";
 import { useMutation } from "@tanstack/react-query";
 import { upLoadFile } from "actions/storageAction";
 import { queryClient } from "config/ReactQueryClientProvider";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function FileInput() {
   const fileRef = useRef(null);
+
+  // 드래그 앤 드롭 함수
+  const onDrop = useCallback(async (acceptedFiles) => {
+    if (acceptedFiles.length > 0) {
+      const formData = new FormData();
+
+      acceptedFiles.forEach((file) => {
+        formData.append(file.name, file);
+      });
+      const result = await uploadImageMutation.mutate(formData);
+      console.log(result);
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    multiple: true,
+  });
+
   const uploadImageMutation = useMutation({
     mutationFn: upLoadFile,
     onSuccess: () => {
@@ -31,16 +51,19 @@ export default function FileInput() {
   };
 
   return (
-    <form
-      onSubmit={fileSubmitHandler}
-      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center"
+    <div
+      {...getRootProps()}
+      //   onSubmit={fileSubmitHandler}
+      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center cursor-pointer"
     >
-      <input ref={fileRef} type="file" className="" />
-      <p>파일을 끌어다 놓거나 클릭하여 업로드 하세요.</p>
-
-      <Button loading={uploadImageMutation.isPending} type="submit">
-        file upload
-      </Button>
-    </form>
+      <input {...getInputProps()} />
+      {uploadImageMutation.isPending ? (
+        <Spinner />
+      ) : isDragActive ? (
+        <p>파일을 놓아주세요.</p>
+      ) : (
+        <p>파일을 끌어다 놓거나 클릭하여 업로드 하세요.</p>
+      )}
+    </div>
   );
 }
