c9bc5e57f4ce6c86f863cd752276945817919b08
yeowonNa <mhyh716@daum.net>
2024-10-14 15:30:10 +0900

    검색 버튼 클릭 시 데이터 패치되도록 수정

--- netfilx-clone/components/header.tsx
+++ netfilx-clone/components/header.tsx
@@ -3,9 +3,21 @@
 import { useRecoilState } from "recoil";
 import Logo from "./logo";
 import { searchState } from "utils/supabase/recoil/atoms";
+import { useState } from "react";
 
 export default function Header() {
-  const [search, setSearch] = useRecoilState(searchState);
+  const [, setSearch] = useRecoilState(searchState);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSearchClick = () => {
+    setSearch(searchInput);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearchClick();
+    }
+  };
 
   return (
     <header className="fixed top-0 left-0 right-0 px-4 py-2 bg-gray-900 flex items-center justify-between z-50">
@@ -18,12 +30,13 @@ export default function Header() {
       </nav>
 
       <div className="flex w-full max-w-72 gap-2 items-center border border-white text-white bg-transparent rounded-md p-2">
-        <i className="fas fa-search" />
+        <i className="fas fa-search" onClick={handleSearchClick} />
         <input
           className="bg-transparent"
           placeholder="Search Movies"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
       </div>
     </header>
