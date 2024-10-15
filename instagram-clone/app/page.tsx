980dd3537672622167318ddd4da9568e05c42431
yeowonNa <mhyh716@daum.net>
2024-10-15 13:16:07 +0900

    git init

---
+++ instagram-clone/app/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Insta",
+  description: "instagram clone",
+};
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Instagram
+    </main>
+  );
+}
